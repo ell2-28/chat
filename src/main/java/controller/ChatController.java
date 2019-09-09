4fddc2d838c8d04078ad0d4768b8f23cc7a4ed9c
ell2-228 <qwerty4383302>
2019-09-09 22:17:20 +0300

    Теперь post передает сообщение в body

--- src/main/java/controller/ChatController.java
+++ src/main/java/controller/ChatController.java
@@ -1,43 +1,29 @@
 package controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.xml.ws.Holder;
 import java.util.ArrayList;
 
 @RestController
 public class ChatController {
 
-//    @GetMapping(value = "/greeting")
-//    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-//        model.addAttribute("name", name);
-//        return "greeting";
-//    }
+    private ArrayList<Holder> list = new ArrayList<>();
 
-    private ArrayList<String> list = new ArrayList<>();
-
-//    @GetMapping("/sendMessage")
-//    @ResponseStatus(HttpStatus.OK)
-//    public String sendMessage(@RequestParam(name = "text", required = false, defaultValue = "Default_Message") String name, Model model) {
-//        list.add(name);
-//        model.addAttribute("text", name);
-//        return "sendMessage";
-//    }
-
-    @RequestMapping(value = "/sendMessage", method = RequestMethod.POST)
+    @RequestMapping(value = "/sendMessage", method = RequestMethod.POST, consumes = {"application/json ; charset=utf-8"})
     @ResponseStatus(HttpStatus.OK)
-    public String sendMessage(@RequestParam(name = "text", required = false, defaultValue = "Default_Message") String name, Model model) {
-        list.add(name);
-        model.addAttribute("text", name);
+    //public String sendMessage(@RequestParam(name = "text", required = false, defaultValue = "Default_Message") String name, Model model) {
+    @ResponseBody
+    public String sendMessage(@RequestBody Holder holder) {
+        list.add(holder);
         return "Message sent";
     }
 
     @GetMapping(value = "/getMessage")
-    public ArrayList<String> getMessage() {
-        for (String s : list) {
-            System.out.println(s);
+    public ArrayList<Holder> getMessage() {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(i);
         }
         return list;
     }
